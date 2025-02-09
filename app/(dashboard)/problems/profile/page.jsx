0ae1a7633d5e7c08979bb3f7c0b56437d615beff
dipakbiswa <dbiswakarma003@gmail.com>
2025-02-10 01:05:11 +0530

    Fix: Wrapped useSearchParams in Suspense

--- app/(dashboard)/problems/profile/page.jsx
+++ app/(dashboard)/problems/profile/page.jsx
@@ -2,8 +2,8 @@ import LeetcodeProfile from "@/components/LeetcodeProfile";
 import { LeetCode } from "leetcode-query";
 import { getCurrentUser } from "@/lib/getCurrentUser";
 export default async function ProfilePage({ params }) {
-  const user = await getCurrentUser();
+  const users = await getCurrentUser();
   const leetcode = new LeetCode();
-  const leetcode_user = await leetcode.user(user?.leetcode_username);
+  const leetcode_user = await leetcode.user(users?.leetcode_username);
   return <LeetcodeProfile data={leetcode_user} />;
 }
