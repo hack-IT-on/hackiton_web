f484759ad722f76aa39c72c399832535be7dbd30
dipakbiswa <dbiswakarma003@gmail.com>
2025-02-10 00:28:37 +0530

    Fix: Wrapped useSearchParams in Suspense

--- app/(dashboard)/projects/new/page.jsx
+++ app/(dashboard)/projects/new/page.jsx
@@ -2,6 +2,6 @@ import AddNewProject from "@/components/AddNewProject";
 import { getCurrentUser } from "@/lib/getCurrentUser";
 export default async function NewProjectPage() {
   const user = await getCurrentUser();
-  if (user.upload_project) return <AddNewProject />;
+  if (user?.upload_project) return <AddNewProject />;
   else return <center>Your are not allow to add project.</center>;
 }
