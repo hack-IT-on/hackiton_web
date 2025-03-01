3136c340d552ab3b4e8cd1dcfe6a8de8ad119c99
dipakbiswa <dbiswakarma003@gmail.com>
2025-03-01 20:38:01 +0530

    Fix: Fix register

--- util/db.js
+++ util/db.js
@@ -3,6 +3,7 @@ import mysql from "mysql2/promise";
 // Create the connection to database
 export const connection = await mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306, // Default MySQL port is 3306
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
