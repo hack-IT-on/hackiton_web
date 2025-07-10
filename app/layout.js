c7f88b8f4ca584cc900bdfe064ff74416eb095fb
dipakbiswa <dbiswakarma003@gmail.com>
2025-07-10 13:41:47 +0530

    Fix: Google search console update

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,61 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Hack It On",
-  description: "Hack It On",
+  description:
+    "Hack It On - Empowering the next generation of developers through community-driven learning.",
+  keywords: [
+    "hack it on",
+    "bit",
+    "bengal institute of technology",
+    "college",
+    "software",
+    "programming",
+  ],
+  authors: [{ name: "Hack It On Team" }],
+  creator: "Hack It On",
+  publisher: "Hack It On",
+  metadataBase: new URL("https://hackiton.vercel.app/"), // Replace with your actual domain
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    title: "Hack It On",
+    description:
+      "Hack It On - Empowering the next generation of developers through community-driven learning..",
+    url: "https://hackiton.vercel.app/", // Replace with your actual domain
+    siteName: "Hack It On",
+    images: [
+      {
+        url: "https://hackiton.vercel.app/logo.png", // Add your OpenGraph image
+        width: 1200,
+        height: 630,
+        alt: "Hack It On - Empowering the next generation of developers through community-driven learning.",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Hack It On",
+    description:
+      "Hack It On - Empowering the next generation of developers through community-driven learning.",
+    images: ["https://hackiton.vercel.app/logo.png"], // Add your Twitter card image
+    creator: "@Hack_It_On", // Replace with your actual Twitter handle
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default async function RootLayout({ children }) {
